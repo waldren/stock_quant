5583b6569c92fcb38ba4cbc818e90003668136d5
Steven Waldren <swaldren@gmail.com>
2022-12-07 20:59:23 -0600

    removed venv scripts

--- .stockquant/Scripts/tda-order-codegen.py
+++
@@ -1,6 +0,0 @@
-#!F:\gitrepo\stock_quant\.stockquant\Scripts\python.exe
-from tda.scripts.orders_codegen import latest_order_main
-
-if __name__ == '__main__':
-    import sys
-    sys.exit(latest_order_main(sys.argv[1:]))
